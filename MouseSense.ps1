052d251b55852a1c2b4312cd78d086e0a47460d0
barbagd <135290539+barbagd@users.noreply.github.com>
2023-12-11 11:54:24 +1100

    Update MouseSense.ps1

--- MouseSense.ps1
+++ MouseSense.ps1
@@ -1,6 +1,17 @@
-# Set the mouse sensitivity to 8
-$mouseSensitivity = 5
 
-# Set the mouse sensitivity registry key value
-$regPath = "HKCU:\Control Panel\Mouse"
-Set-ItemProperty -Path $regPath -Name MouseSensitivity -Value $mouseSensitivity
+# Define the registry key path and value
+$regKeyPath = "Control Panel\Mouse"
+$regValueName = "MouseSensitivity"
+$regValueData = "5"
+
+# Get all user profiles under HKEY_USERS
+$userProfiles = Get-Item -LiteralPath "Registry::HKEY_USERS\*" -ErrorAction SilentlyContinue
+
+# Iterate through each user profile and set the registry key
+foreach ($userProfile in $userProfiles.PSChildName) {
+    $regPath = "Registry::HKEY_USERS\$userProfile\$regKeyPath"
+    New-Item -Path $regPath -Force | Out-Null
+    Set-ItemProperty -Path $regPath -Name $regValueName -Value $regValueData
+}
+
+Write-Host "Registry keys set for all user profiles."
